7c934a43add4e3b500031c08a67610a2941b9375
mmpechenbku <mmpechenbku@gmail.com>
2023-10-22 23:14:18 +0300

    renaming project

--- SaltToTasteProject/SalToTasteProject/asgi.py
+++ SaltToTasteProject/SalToTasteProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SalToTasteProject project.
+ASGI config for SaltToTasteProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SalToTasteProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SaltToTasteProject.settings')
 
 application = get_asgi_application()
--- SaltToTasteProject/SalToTasteProject/wsgi.py
+++ SaltToTasteProject/SalToTasteProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SalToTasteProject project.
+WSGI config for SaltToTasteProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SalToTasteProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SaltToTasteProject.settings')
 
 application = get_wsgi_application()

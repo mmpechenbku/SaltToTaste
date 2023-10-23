f3d402c9522bbc9b37c9441c5c5a7236205d6480
Zykawoo <kirillsavcin@inbox.ru>
2023-10-24 01:30:45 +0300

    IconsRework

--- SaltToTasteProject/SaltToTasteProject/settings.py
+++ SaltToTasteProject/SaltToTasteProject/settings.py
@@ -128,7 +128,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [BASE_DIR / 'SaltToTasteProject/static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'SaltToTasteProject/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

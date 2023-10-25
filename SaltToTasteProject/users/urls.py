75572e92070f3920a390eeceac935a6e3f169b00
mmpechenbku <mmpechenbku@gmail.com>
2023-10-25 21:54:39 +0300

    sign-in and sign-up is working now

--- SaltToTasteProject/users/urls.py
+++ SaltToTasteProject/users/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-
-from SaltToTasteProject import settings
-from users.views import SignUpView, LoginUser
+# import SaltToTasteProject.SaltToTasteProject.settings
+from .views import SignUpView, LoginUser
 from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('registration/', SignUpView.as_view(), name='registrations'),
+    path('registration/', SignUpView.as_view(), name='registration'),
     path('login/', LoginUser.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout')
+    # path('logout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout')
 
 ]

11d05bdf517c3b954f237cf6ccbe77c57e0628f2
kirill <kirillsavcin@inbox.ru>
2023-11-11 03:50:27 +0300

    add div for recipe_detail

--- SaltToTasteProject/recipes/urls.py
+++ SaltToTasteProject/recipes/urls.py
@@ -7,6 +7,7 @@ recipes_urls = [
     path('collections/', collections, name='collections'),
     path('save_recipe/', SaveRecipeCreateView.as_view(), name='save_recipe'),
     path('recipe/<int:pk>/comments/create/', CommentCreateView.as_view(), name='comment_create_view'),
+   #path('recipe/', recipe_detail, name='rec'),
     path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
     path('add_recipe/', add_recipe, name='add_recipe'),
     path('api/ingredients/', IngredientSearchView.as_view(), name='ingredient-search'),
